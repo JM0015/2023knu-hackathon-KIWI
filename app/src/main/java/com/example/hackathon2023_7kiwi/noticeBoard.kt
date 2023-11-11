316980e3abde56cf551fa34cc43893a175ad61e8
jisuSong0625 <108786142+jisuSong0625@users.noreply.github.com>
2023-11-11 18:49:11 +0900

    .

--- app/src/main/java/com/example/hackathon2023_7kiwi/noticeBoard.kt
+++ app/src/main/java/com/example/hackathon2023_7kiwi/noticeBoard.kt
@@ -2,17 +2,15 @@ package com.example.hackathon2023_7kiwi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.hackathon2023_7kiwi.databinding.ActivityNoticeBoardBinding
 import com.example.hackathon2023_7kiwi.databinding.ItemMainBinding
 
-//var dataList = mutableListOf<datum>()
+val dataList = mutableListOf<Datum>()
 
 class noticeBoard : AppCompatActivity() {
 
@@ -24,24 +22,26 @@ class noticeBoard : AppCompatActivity() {
         //supportActionBar?.setTitle("Title")
 
         var data: Datum
-        var dataList = mutableListOf<Datum>()
+        //val dataList = mutableListOf<Datum>()
         for (i in 1..10) {
-            data = Datum(i, "title ${i}", "content ${i}")
-            dataList.add(data)
+            data = Datum("$i", "title $i", "content $i")
+            dataList.add(i, data)
         }
+        /*val a = Datum("0", "title 0", "content 0")
+        dataList.add(0, a)*/
 
-        printRecycler(dataList)
-    }
+        //printRecycler(dataList)
 
-    private fun printRecycler(dataList: MutableList<Datum>) {
-        // 1. 데이터를 불러온다
-        //val data = dataList
-        // 2. adapter 를 생성
-        val customAdapter = CustomAdapter(dataList)
-        // 3. 화면의 RecyclerView 와 연결
-        binding.recyclerView.adapter = customAdapter
-        // 4. 레이아웃 매니저 설정
-        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        //fun printRecycler(dataList: MutableList<Datum>) {
+            // 1. 데이터를 불러온다
+            //val data = dataList
+            // 2. adapter 를 생성
+            val customAdapter = CustomAdapter(dataList)
+            // 3. 화면의 RecyclerView 와 연결
+            binding.recyclerView.adapter = customAdapter
+            // 4. 레이아웃 매니저 설정
+            binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        //}
     }
 }
 
@@ -61,16 +61,14 @@ class CustomAdapter(private val dataList: MutableList<Datum>) : RecyclerView.Ada
 
     override fun getItemCount() = dataList.size
 
-    class Holder(val binding: ItemMainBinding) : RecyclerView.ViewHolder(binding.root) {
+    class Holder(val binding: ItemMainBinding) : ViewHolder(binding.root) {
         // 3. 받은 데이터를 화면에 출력한다.
         fun setTry(datum: Datum) {
             with(binding){
-                index.text = "${datum.index}"
-                itemTitle.text = "${datum.titles}"
-                itemContent.text = "${datum.contents}"
+                index.text = datum.index
+                itemTitle.text = datum.titles
+                itemContent.text = datum.contents
             }
         }
     }
 }
-
-data class Datum(var index: Int, var titles: String, var contents: String)
